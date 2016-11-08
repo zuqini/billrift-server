bd0844a543e20a9a8d4ba7f17912ea4e6c42b9bc
zuqini <lizuqiliang@gmail.com>
2016-11-08 17:44:09 -0500

    set up initial project

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+// var mongoose = require('mongoose');
+
+// var db = require('./config/db.js');
+
+// mongoose.connect(db.url);
+
+require('./app/routes')(app);
+
+module.exports = app;
