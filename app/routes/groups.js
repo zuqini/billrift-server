a7846636474a0264847ac5278fdd063df884627a
zuqini <lizuqiliang@gmail.com>
2016-11-09 19:59:17 -0500

    disruptive balance logic

--- app/routes/groups.js
+++ app/routes/groups.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 var Group = require('../models/group');
 var User = require('../models/user');
+var Transaction = require('../models/transaction');
 
 router.get('/', function(req, res) {
     User.findOne({ googleId: req.user.googleId }, function(err, user) {
@@ -13,7 +14,26 @@ router.get('/', function(req, res) {
         };
         Group.find(query, function(err, groups) {
             if (err) return res.status(500).json({ status: 500, error: err.toString()});
-            res.status(200).json(groups);
+            var groupIds = _.map(groups, 'id');
+            Transaction.find({groupId: {$in: groupIds}}, function(err, transactions){
+                if (err) return res.status(500).json({ status: 500, error: err.toString()});
+                var transactionsByGroup = _.groupBy(transactions, function(transaction) {
+                    return transaction.groupId;
+                });
+                var groupsWithBalances = _.map(groups, function(group) {
+                    var balance = 0;
+                    _.forEach(transactionsByGroup[group.id], function(transaction) {
+                        if (req.user.googleId.localeCompare(transaction.userFromId) === 0) {
+                            balance += transaction.amount;
+                        } else if (req.user.googleId.localeCompare(transaction.userToId) === 0) {
+                            balance -= transaction.amount;
+                        }
+                    });
+                    return {balance:balance, group: group} ;
+                });
+
+                res.status(200).json(groupsWithBalances);
+            });
         });
     });
 });
