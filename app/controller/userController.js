8453908ba4ad80d2624699965dff1d96dc1399d8
zuqini <lizuqiliang@gmail.com>
2016-11-26 17:19:24 -0500

    more restructure

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
+var Group = require('../models/group');
 var User = require('../models/user');
+var Transaction = require('../models/transaction');
+
+var _ = require('lodash');
 
 module.exports = {
 	login(req, res) {
@@ -21,5 +22,43 @@ module.exports = {
 			if (err) res.status(500).json({ status: 500, error: err.toString()});
 			res.status(200).json(users);
 		});
+	},
+	getGroups(req, res) {
+		User.findOne({ googleId: req.user.googleId }, function(err, user) {
+        if (err) return res.status(500).json({ status: 500, error: err.toString()});
+        var query = {
+            id: { $in: user.groupIds }
+        };
+        Group.find(query, function(err, groups) {
+            if (err) return res.status(500).json({ status: 500, error: err.toString()});
+            var groupIds = _.map(groups, 'id');
+            Transaction.find({groupId: {$in: groupIds}}, function(err, transactions){
+                if (err) return res.status(500).json({ status: 500, error: err.toString()});
+                var transactionsByGroup = _.groupBy(transactions, function(transaction) {
+                    return transaction.groupId;
+                });
+                var groupsWithBalances = _.map(groups, function(group) {
+                    var balance = 0;
+                    _.forEach(transactionsByGroup[group.id], function(transaction) {
+                        if (req.user.googleId.localeCompare(transaction.userFromId) === 0) {
+                            balance += transaction.amount;
+                        } else if (req.user.googleId.localeCompare(transaction.userToId) === 0) {
+                            balance -= transaction.amount;
+                        }
+                    });
+                    return {
+                        _id: group._id,
+                        __v: group.__v,
+                        balance: balance,
+                        id: group.id,
+                        name: group.name,
+                        userIds: group.userIds
+                    } ;
+                });
+
+                res.status(200).json(groupsWithBalances);
+            });
+        });
+    });
 	}
 };
--- app/routes/groups.js
+++ app/routes/groups.js
@@ -1,48 +1,10 @@
 var express = require('express');
-var _ = require('lodash');
 var router = express.Router();
 
-var Group = require('../models/group');
-var User = require('../models/user');
-var Transaction = require('../models/transaction');
+var userController = require('../controller/userController');
 
 router.get('/', function(req, res) {
-    User.findOne({ googleId: req.user.googleId }, function(err, user) {
-        if (err) return res.status(500).json({ status: 500, error: err.toString()});
-        var query = {
-            id: { $in: user.groupIds }
-        };
-        Group.find(query, function(err, groups) {
-            if (err) return res.status(500).json({ status: 500, error: err.toString()});
-            var groupIds = _.map(groups, 'id');
-            Transaction.find({groupId: {$in: groupIds}}, function(err, transactions){
-                if (err) return res.status(500).json({ status: 500, error: err.toString()});
-                var transactionsByGroup = _.groupBy(transactions, function(transaction) {
-                    return transaction.groupId;
-                });
-                var groupsWithBalances = _.map(groups, function(group) {
-                    var balance = 0;
-                    _.forEach(transactionsByGroup[group.id], function(transaction) {
-                        if (req.user.googleId.localeCompare(transaction.userFromId) === 0) {
-                            balance += transaction.amount;
-                        } else if (req.user.googleId.localeCompare(transaction.userToId) === 0) {
-                            balance -= transaction.amount;
-                        }
-                    });
-                    return {
-                        _id: group._id,
-                        __v: group.__v,
-                        balance: balance,
-                        id: group.id,
-                        name: group.name,
-                        userIds: group.userIds
-                    } ;
-                });
-
-                res.status(200).json(groupsWithBalances);
-            });
-        });
-    });
+    return userController.getGroups(req, res);
 });
 
 module.exports = router;
