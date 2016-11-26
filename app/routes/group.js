72b5b090c26c846581c6b4ce0f202aba9aa609de
Zal Machado <zalmachado@gmail.com>
2016-11-26 17:24:48 -0500

    add some fucntions to group controller

--- app/routes/group.js
+++ app/routes/group.js
@@ -29,97 +29,25 @@ router.post('/', function(req, res) {
  *  Get the list of users for a group identified by id
  */
 router.get('/:id/users', function (req, res) {
-    var groupId = req.params.id;
-    Group.findOne({id: groupId}, function(err, group) {
-        if (err) return res.status(500).json({status: 500, error: err.toString()});
-        var userIds = group.userIds;
-        var query = {
-            googleId: {$in: userIds}
-        };
-        User.find(query, function (err, users) {
-            if (err) return res.status(500).json({ status: 500, error: err.toString()});
-            res.status(200).json(users);
-        });
-    });
+    return groupController.getUsers(req, res);
 });
 
 router.post('/:id/user', function (req, res) {
-    var groupId = req.params.id;
-    var email = req.body.email;
-    User.findOneAndUpdate({email: email}, {$push: { groupIds : parseInt(groupId, 10) }}, {new: true}, function(err, user) {
-        if (err) return res.status(500).json({ status: 500, error: err.toString()});
-        if (!user) return res.status(404).json({ status: 404, error: 'User not found.'});
-        var googleId = user.googleId;
-        Group.findOne({id: groupId}, function(err, group) {
-            if (err) return res.status(500).json({ status: 500, error: err.toString()});
-            if (!group) return res.status(404).json({ status: 404, error: 'Group not found.'});
-            if (_.indexOf(group.userIds, googleId) !== -1) return res.status(500).json({ status: 500, error: "User is already in group."});
-
-            Group.update({id: groupId}, {$push: {userIds: googleId}}, function(err, group) {
-                if (err) return res.status(500).json({ status: 500, error: err.toString()});
-                res.status(200).json({});
-            });
-        });
-    });
+    return groupController.addUserToGroup(req, res);
 });
 
 /*
  *  Get the list of transactions for a group identified by id
  */
 router.get('/:id/transactions', function (req, res) {
-    var groupId = req.params.id;
-
-    if (!groupId) {
-        return res.status(400).json({ status: 400, error: "Bad parameters."});
-    }
-
-    var query = {
-        groupId: groupId
-    };
-    Transaction.find(query, function(err, transactions) {
-        if (err) return res.status(500).json({ status: 500, error: err.toString()});
-        res.json(transactions);
-    });
+    return groupController.getTransactions(req, res);
 });
 
 /*
  *  Add a new transaction to the group
  */
 router.post('/:id/transaction', function(req, res) {
-    var groupId = req.params.id;
-    var userFromId = req.body.userFromId;
-    var userToId = req.body.userToId;
-    var amount = req.body.amount;
-    var title = req.body.title;
-
-    if (!groupId || !userFromId || !userToId || !amount) {
-        return res.status(400).json({ status: 400, error: "Bad parameters."});
-    }
-
-    if (userFromId.localeCompare(userToId) === 0) {
-        return res.status(400).json({ status: 400, error: "Cannot have a transaction with the same user."});
-    }
-
-    Group.findOne({id: groupId}, function(err, group) {
-        if (err) return res.status(500).json({ status: 500, error: err.toString()});
-        if (!group) return res.status(404).json({ status: 404, error: "Group not found."});
-        if (group.userIds.indexOf(userFromId) === -1 || group.userIds.indexOf(userToId) === -1) {
-            return res.status(404).json({ status: 404, error: "User not found."});
-        }
-        Transaction.create({
-            userFromId: userFromId,
-            userToId: userToId,
-            amount: amount,
-            groupId: groupId,
-            title: title
-        }, function(err, transaction) {
-            if (!err) {
-                res.status(200).json({transaction: transaction});
-            } else {
-                res.status(500).json({error: err});
-            }
-        });
-    });
+    return groupController.addTransactionToGroup(req, res);
 });
 
 router.get('/:id/balances', function (req, res) {
